be4c94fadea841495022f51376be77e4e6b6ba26
Pere Gilabert <perecasxiru@hotmail.com>
2019-10-11 17:58:30 +0200

    m

--- FooApp/models.py
+++ FooApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    prod_id = models.CharField(unique=True, default=uuid.uuid4, max_length=999)
+    prod_id = models.CharField(unique=True, default=uuid.uuid4, max_length=1000)
     name = models.CharField(default="", max_length=300)
     description = models.TextField(default="Description here", max_length=999)
     price = models.DecimalField(max_digits=6, decimal_places=2)
